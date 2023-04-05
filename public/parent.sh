90a578b32444e290792e3fe5fa8d49fca6980f0e
parkito <artyom-karnov@yandex.ru>
2023-04-04 20:28:50 -0600

    common bash logging

--- public/parent.sh
+++ public/parent.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "THIS IS COMMON"
+function logInfo() {
+  echo "INFO: $(date +"%T") $1"
+}
+
+function logInfo() {
+  echo "DEBUG: $(date +"%T") $1"
+}
+
+function logWarn() {
+  echo "$(tput setaf 1)WARN: $1$(tput sgr0)"
+}
