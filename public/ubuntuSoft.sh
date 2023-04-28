f934bb83520bc7d8d4ebb6efc379cad8cdb91834
parkito <artyom-karnov@yandex.ru>
2023-04-27 21:05:49 -0600

    add env configs to ubuntu soft

--- public/ubuntuSoft.sh
+++ public/ubuntuSoft.sh
@@ -13,3 +13,18 @@ function upgradeAllPackages() {
   sudo apt-get upgrade -y
   sudo apt autoremove -y
 }
+
+function setupEnvConfigs() {
+  logInfo "System wide configuration"
+  echo EDITOR=vim >>/etc/environment
+}
+
+function magicKeyboard() {
+  log "System wide configuration"
+  echo options hid_apple fnmode=2 | sudo tee -a /etc/modprobe.d/hid_apple.conf
+  sudo update-initramfs -u -k all
+}
+
+function doReboot() {
+  sudo reboot
+}
